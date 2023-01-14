43141ef0b2ca75d07e7fb1ee15d7d8266fa8b541
sanodzegg <sanodzegg@gmail.com>
2023-01-14 17:03:37 +0400

    add collapse functionality to FAQ

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,21 @@ const init = () => {
 }
 
 init();
+
+const initFAQ = () => {
+    const collapse = document.querySelectorAll(".collapseFAQ");
+    collapse.forEach((el, i) => {
+        el.addEventListener("click", () => handleQuestionWrappers(i));
+    });
+
+    const handleQuestionWrappers = (index) => {
+        const questionWrappers = document.querySelectorAll(".questionWrapper");
+        
+        questionWrappers.forEach(el => {
+            el.classList.remove("active");
+        });
+        questionWrappers[index].classList.add("active");
+    }
+}
+
+initFAQ();
