ba5bdeb9c46b6f08238abbaed5a95b3f6fabafe0
sanodzegg <sanodzegg@gmail.com>
2023-01-15 22:35:38 +0400

    add navbar and FAQ navigation functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,43 @@
-const init = () => {
-    console.log(true);
-}
-
-init();
-
 const initFAQ = () => {
     const collapse = document.querySelectorAll(".collapseFAQ");
-    collapse.forEach((el, i) => {
-        el.addEventListener("click", () => handleQuestionWrappers(i));
-    });
 
-    const handleQuestionWrappers = (index) => {
-        const questionWrappers = document.querySelectorAll(".questionWrapper");
+    collapse.forEach((el, i) => {
+        el.addEventListener("click", () => {
+            const questionWrappers = document.querySelectorAll(".questionWrapper");
         
-        questionWrappers.forEach(el => {
-            el.classList.remove("active");
+            questionWrappers.forEach(el => {
+                el.classList.remove("active");
+            });
+            
+            questionWrappers[i].classList.add("active");
         });
-        questionWrappers[index].classList.add("active");
-    }
+    });
 }
 
 initFAQ();
+
+const initNavbar = () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = [...document.querySelector(".navlist").children]; // HTMLCollection --> Array
+
+    navLinks.forEach((el, i) => {
+        el.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            navLinks.forEach(e => {
+                e.firstChild.classList.remove("active");
+            });
+    
+            const anchor = el.firstChild;
+            anchor.classList.add("active");
+    
+            const y = sections[i].offsetTop;
+            window.scrollTo({
+                top: y,
+                behavior: "smooth"
+            });
+        });
+    });
+}
+
+initNavbar();
